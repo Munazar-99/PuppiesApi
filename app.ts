48d274603d3cd4989cb46f76a4bcf63db7cfad55
Munazar Abdulle <munazar.ali.001@gmail.com>
2023-01-07 22:23:04 +0100

    Endpoints working.I will work on optimization

--- app.ts
+++ app.ts
@@ -1,10 +1,48 @@
 import express from 'express';
 import { Request, Response, Application } from 'express';
+import {puppies} from './data/database'
 
 const app: Application = express();
 
-app.get('/api/test', (_req: Request, res: Response) => {
-  return res.status(200).json({ test: 'is working as it should' });
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/puppies', (_req: Request, res: Response) => {
+  return res.status(200).json(puppies);
+});
+app.get('/puppies/:id', (_req: Request, res: Response) => {
+  const puppyId = _req.params.id;
+  const requestedPuppy = puppies.find(puppy => puppy.id ===Number(puppyId))
+  return res.status(200).json(requestedPuppy);
+});
+app.post('/puppies', (req: Request, res: Response) => {
+  const {name, breed, DOB}: { name: string; breed: string; DOB:string } = req.body;
+  const newPuppy ={
+    id: Date.now(),
+    name: name,
+    breed: breed,
+    DOB: DOB
+  }
+  puppies.push(newPuppy);
+  return res.status(200).send(puppies);
+});
+app.put('/api/puppies/:id', (req: Request, res: Response) => {
+  const puppyId = req.params.id;
+  const requestedPuppyId = puppies.findIndex(puppy => puppy.id ===Number(puppyId))
+  const {name, breed, DOB}: { name: string; breed: string; DOB:string } = req.body;
+  console.log(requestedPuppyId)
+  if( requestedPuppyId >= 0 && name && breed && DOB) {
+    console.log('hi')
+    puppies[requestedPuppyId]!.name =  name;
+    puppies[requestedPuppyId]!.breed = breed;
+    puppies[requestedPuppyId]!.DOB = DOB;
+  }
+  return res.status(200).json(puppies);
+});
+app.delete('/api/puppies/:id', (_req: Request, res: Response) => {
+  const puppyId = _req.params.id;
+  const requestedPuppy = puppies.filter(puppy => puppy.id !==Number(puppyId))
+  return res.status(200).json(requestedPuppy);
 });
 
 export default app;
---
+++ data/database.ts
@@ -0,0 +1,14 @@
+export const puppies = [
+    {
+        id: 1,
+        breed:'papa johnes',
+        name:'doggy doggy',
+        DOB: '2 April 2001'
+    },
+    {
+        id: 2,
+        breed:'wolf',
+        name:'boyyy',
+        DOB: '2 June 2020'
+    },
+]
